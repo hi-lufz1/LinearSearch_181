57372fb1f30fd5f109aec68233d88b4a1c6b55ea
hi-lufz1 <latifufauzi@gmail.com>
2023-04-14 10:32:21 +0700

    Add project files.

---
+++ linearsearch/linearsearch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int arr[20] // array to be searched
+int n; // number of element in the array
+int i; // index pf array element
+
+
+void input()
+{
+	while (true)
+
+
+
+}
+
+
