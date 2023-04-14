48489292749e6ff5ae579aee191a01e22acb46a0
hi-lufz1 <latifufauzi@gmail.com>
2023-04-14 10:34:39 +0700

    1

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int arr[20] // array to be searched
+int arr[20]; // array to be searched
 int n; // number of element in the array
 int i; // index pf array element
 
@@ -9,7 +9,13 @@ int i; // index pf array element
 void input()
 {
 	while (true)
+	{
+		cout << "Enter the number of element in the array : ";
+		cin >> n;
+		if ((n > 0) && (n <= 20));
+		break;
 
+	}
 
 
 }
